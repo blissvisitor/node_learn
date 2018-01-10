7fc47c6efa1d69a6123705b14de463164d3cd1c0
blissvisitor <192949421@qq.com>
2018-01-10 18:02:07 +0800

    hello-ws

---
+++ hello-ws/app.js
@@ -0,0 +1,19 @@
+//导入websocket模块
+const WebSocket=require('ws');
+//引用Server类
+const WebServer=WebSocket.Server;
+//实例化
+const wss=new WebServer({
+	port:3000
+});
+wss.on('connection', function (ws) {
+    console.log(`[SERVER] connection()`);
+    ws.on('message', function (message) {
+        console.log(`[SERVER] Received: ${message}`);
+        ws.send(`ECHO: ${message}`, (err) => {
+            if (err) {
+                console.log(`[SERVER] error: ${err}`);
+            }
+        });
+    })
+});
